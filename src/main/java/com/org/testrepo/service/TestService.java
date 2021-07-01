4b5552e93e217c6f002639e1962c5b94b3773d58
Manzil Moharana <mmoharana@tekion.com>
2021-07-01 19:50:06 +0530

    Removed history, due to sensitive data

---
+++ src/main/java/com/org/testrepo/service/TestService.java
@@ -0,0 +1,15 @@
+package com.org.testrepo.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestService {
+
+    public String getResponse() {
+        return "hello from the other side+1";
+    }
+
+    public String getResponse2() {
+        return "hello from the other side pt2";
+    }
+}
